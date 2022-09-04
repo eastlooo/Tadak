0efaffadb2fe6076e0cfdcd0bef9649a59a8d9ef
oluye7 <oluye7@naver.com>
2022-09-04 23:37:30 +0900

    Color Asset 추가

---
+++ Tadak/Utils/Extensions/UIColor++.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor++.swift
+//  Tadak
+//
+//  Created by 정동천 on 2022/09/04.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    class var customLightGray: UIColor? { return UIColor(named: "LightGray") }
+    class var customLightNavy: UIColor? { return UIColor(named: "LightNavy") }
+    class var customNavy: UIColor? { return UIColor(named: "Navy") }
+    class var customDarkNavy: UIColor? { return UIColor(named: "DarkNavy") }
+    class var customCoral: UIColor? { return UIColor(named: "Coral") }
+    class var customPumpkin: UIColor? { return UIColor(named: "Pumpkin") }
+    class var customSkyBlue: UIColor? { return UIColor(named: "SkyBlue") }
+}
