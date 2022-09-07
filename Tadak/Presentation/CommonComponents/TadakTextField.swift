931ee743f49ab80ef2203f926d6df0d7e5abb61f
oluye7 <oluye7@naver.com>
2022-09-07 20:01:56 +0900

    텍스트 필드 붙여넣기 기능 사용 못하게 overriding

--- Tadak/Presentation/CommonComponents/TadakTextField.swift
+++ Tadak/Presentation/CommonComponents/TadakTextField.swift
@@ -27,6 +27,13 @@ final class TadakTextField: UITextField {
     }
     
     // MARK: Helpers
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
+            return false
+        }
+        return super.canPerformAction(action, withSender: sender)
+    }
+    
     private func configure() {
         autocorrectionType = .no
         autocapitalizationType = .none
